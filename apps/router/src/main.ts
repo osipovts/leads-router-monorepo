b221c217f7940be5cb11755158084bea10011391
Osipov <osipov.ts@proton.me>
2026-09-20 20:21:57 +0800

    #18 Remove unused HTTP versioning from router bootstrap

--- apps/router/src/main.ts
+++ apps/router/src/main.ts
@@ -1,4 +1,4 @@
-import { Logger, VersioningType } from '@nestjs/common';
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import type { AppConfigType } from './app.config';
@@ -8,15 +8,8 @@ import { AppModule } from './app.module';
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
 
-  // API version
-  app.enableVersioning({ type: VersioningType.URI });
-  app.setGlobalPrefix('api');
-
-  // Global stuff
-
   const config = app.get<AppConfigType>(appConfig.KEY);
 
-  // Start
   await app.listen(config.port, config.host);
 }
 
